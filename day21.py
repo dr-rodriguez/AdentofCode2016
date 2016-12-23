4fe6f9f08538f67583206745800be28bc6dc9cfc
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2016-12-23 13:01:33 -0500

    solution for day 21

--- day21.py
+++ day21.py
@@ -45,3 +45,139 @@ After these steps, the resulting scrambled password is decab.
 Now, you just need to generate a new scrambled password and you can access the system. Given the list of scrambling
 operations in your puzzle input, what is the result of scrambling abcdefgh?
 """
+
+import re
+import numpy as np
+
+my_input = """swap position 4 with position 0
+swap letter d with letter b
+reverse positions 0 through 4
+rotate left 1 step
+move position 1 to position 4
+move position 3 to position 0
+rotate based on position of letter b
+rotate based on position of letter d
+"""
+
+with open('data/day21_input.txt', 'r') as f:
+    my_input = f.read()
+
+my_input = my_input.strip().split('\n')
+
+# password = list('abcde')
+password = list('abcdefgh')
+
+print(''.join(password))
+for line in my_input:
+    if line.startswith('swap position'):
+        pattern = r'swap position (\d+) with position (\d+)'
+        pos1, pos2 = re.findall(pattern, line)[0]
+        pos1, pos2 = int(pos1), int(pos2)
+        tmp = password[pos1]
+        password[pos1] = password[pos2]
+        password[pos2] = tmp
+    elif line.startswith('reverse'):
+        pattern = r'reverse positions (\d+) through (\d+)'
+        pos1, pos2 = re.findall(pattern, line)[0]
+        pos1, pos2 = int(pos1), int(pos2)
+        tmp = password[pos1:pos2+1][::-1]
+        password[pos1:pos2+1] = tmp
+    elif line.startswith('swap letter'):
+        pattern = r'swap letter (\w+) with letter (\w+)'
+        let1, let2 = re.findall(pattern, line)[0]
+        ind1 = np.where(np.array(password) == let1)[0][0]
+        ind2 = np.where(np.array(password) == let2)[0][0]
+        password[ind2] = let1
+        password[ind1] = let2
+    elif line.startswith('rotate left'):
+        pattern = r'rotate left (\d+) step+'
+        step = int(re.findall(pattern, line)[0])
+        password = np.roll(password, -1*step).tolist()
+    elif line.startswith('rotate right'):
+        pattern = r'rotate right (\d+) step+'
+        step = int(re.findall(pattern, line)[0])
+        password = np.roll(password, step).tolist()
+    elif line.startswith('rotate based'):
+        pattern = r'rotate based on position of letter (\w+)'
+        let = re.findall(pattern, line)[0]
+        ind = np.where(np.array(password) == let)[0][0]
+        rot = 1 + ind
+        if ind >= 4:
+            rot += 1
+        password = np.roll(password, rot).tolist()
+    elif line.startswith('move'):
+        pattern = r'move position (\d+) to position (\d+)'
+        pos1, pos2 = re.findall(pattern, line)[0]
+        pos1, pos2 = int(pos1), int(pos2)
+        let = password.pop(pos1)
+        password.insert(pos2, let)
+
+    print('{}: {}'.format(line, ''.join(password)))
+
+
+print(''.join(password))
+
+"""
+--- Part Two ---
+
+You scrambled the password correctly, but you discover that you can't actually modify the password file on the system.
+You'll need to un-scramble one of the existing passwords by reversing the scrambling process.
+
+What is the un-scrambled version of the scrambled password fbgdceah?
+"""
+
+# Unfortunately, one of the steps can't be easily reversed.
+# Trying instead every permutation of the input strings
+
+from itertools import permutations
+
+for password in permutations('abcdefgh'):
+    orig = list(password)
+    password = list(password)
+    for line in my_input:
+        if line.startswith('swap position'):
+            pattern = r'swap position (\d+) with position (\d+)'
+            pos1, pos2 = re.findall(pattern, line)[0]
+            pos1, pos2 = int(pos1), int(pos2)
+            tmp = password[pos1]
+            password[pos1] = password[pos2]
+            password[pos2] = tmp
+        elif line.startswith('reverse'):
+            pattern = r'reverse positions (\d+) through (\d+)'
+            pos1, pos2 = re.findall(pattern, line)[0]
+            pos1, pos2 = int(pos1), int(pos2)
+            tmp = password[pos1:pos2 + 1][::-1]
+            password[pos1:pos2 + 1] = tmp
+        elif line.startswith('swap letter'):
+            pattern = r'swap letter (\w+) with letter (\w+)'
+            let1, let2 = re.findall(pattern, line)[0]
+            ind1 = np.where(np.array(password) == let1)[0][0]
+            ind2 = np.where(np.array(password) == let2)[0][0]
+            password[ind2] = let1
+            password[ind1] = let2
+        elif line.startswith('rotate left'):
+            pattern = r'rotate left (\d+) step+'
+            step = int(re.findall(pattern, line)[0])
+            password = np.roll(password, -1 * step).tolist()
+        elif line.startswith('rotate right'):
+            pattern = r'rotate right (\d+) step+'
+            step = int(re.findall(pattern, line)[0])
+            password = np.roll(password, step).tolist()
+        elif line.startswith('rotate based'):
+            pattern = r'rotate based on position of letter (\w+)'
+            let = re.findall(pattern, line)[0]
+            ind = np.where(np.array(password) == let)[0][0]
+            rot = 1 + ind
+            if ind >= 4:
+                rot += 1
+            password = np.roll(password, rot).tolist()
+        elif line.startswith('move'):
+            pattern = r'move position (\d+) to position (\d+)'
+            pos1, pos2 = re.findall(pattern, line)[0]
+            pos1, pos2 = int(pos1), int(pos2)
+            let = password.pop(pos1)
+            password.insert(pos2, let)
+    if ''.join(password) == 'fbgdceah':
+        print('Matched fbgdceah = {}'.format(''.join(password)))
+        print('For: {}'.format(''.join(orig)))
+        break
